b72b597478a26947eb61fd005ff44bf64445c516
DavidePantea <davide.pantea22@gmail.com>
2023-10-13 18:23:54 +0300

    Firs two problems done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Methode notenProzessor = new Methode();
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int[] noten = new int[] { 10, 20, 30, 39, 47, 48, 60 };
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println(Arrays.toString(Methode.nichtAusreichendeNoten(noten)));
+        System.out.println(Methode.average(noten));
+        System.out.println(Arrays.toString(Methode.allRounded(noten)));
+        System.out.println(Methode.maxRounded(noten));
+
+        System.out.println(Zahlen.minNumber(noten));
+        System.out.println(Zahlen.maxNumber(noten));
     }
 }
---
+++ src/Zahlen.java
@@ -0,0 +1,45 @@
+public class Zahlen {
+    public static int maxNumber(int[] array){
+        int max = 0;
+        for(int i = 0; i < array.length; i++){
+            if(array[i] > max ){
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static int minNumber(int[] array){
+        int min = array[0];
+        for(int i = 0; i < array.length; i++){
+            if(array[i] < min ){
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static int bigger(int[] array){
+        int min = minNumber(array);
+        int big = 0;
+        for(int i = 0; i < array.length; i++){
+            big += array[i];
+            if(array[i] == min){
+                continue;
+            }
+        }
+        return big;
+    }
+
+    public static int smaller(int[] array){
+        int max = maxNumber(array);
+        int big = 0;
+        for(int i = 0; i < array.length; i++){
+            big += array[i];
+            if(array[i] == max){
+                continue;
+            }
+        }
+        return big;
+    }
+}

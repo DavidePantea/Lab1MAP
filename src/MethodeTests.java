a07b2b1a5e4cff5c6c5f1149acb5b3211a850b18
DavidePantea <davide.pantea22@gmail.com>
2023-11-03 13:38:46 +0200

    Added Operation tests

--- src/MethodeTests.java
+++ src/MethodeTests.java
@@ -2,23 +2,50 @@ import java.util.Arrays;
 
 public class MethodeTests {
     public static void testNichtAusreichendeNoten() {
-        int[] grades = {45, 53, 62, 37, 88, 95};
-        int[] badGrades = Methode.nichtAusreichendeNoten(grades);
-        assert Arrays.equals(badGrades, new int[]{37}) : "Test failed for nichtAusreichendeNoten (expected case)";
+        // Test case with some failing grades
+        int[] grades1 = {45, 53, 62, 37, 88, 95};
+        int[] badGrades1 = Methode.nichtAusreichendeNoten(grades1);
+        assert Arrays.equals(badGrades1, new int[]{37}) : "Test failed for nichtAusreichendeNoten (expected case)";
 
+        // Test case with all passing grades
         int[] allPassingGrades = {75, 80, 90, 85};
         int[] unexpFailingGrades = Methode.nichtAusreichendeNoten(allPassingGrades);
         assert unexpFailingGrades.length == 0 : "Test failed for nichtAusreichendeNoten (unexpected case)";
     }
 
     public static void testAverage() {
+        // Test case with some grades
         int[] allGrades = {80, 75, 90, 65, 92};
         int avg = Methode.average(allGrades);
         assert avg == 80 : "Test failed for average (expected case)";
 
+        // Test case with no grades
         int[] emptyGrades = {};
         int unexpectedAvg = Methode.average(emptyGrades);
         assert unexpectedAvg == 0 : "Test failed for average (unexpected case)";
     }
-}
 
+    public static void testAllRounded() {
+        // Test case with some grades
+        int[] grades = {45, 53, 62, 37, 88, 95};
+        int[] roundedGrades = Methode.allRounded(grades);
+        assert Arrays.equals(roundedGrades, new int[]{45, 55, 60, 37, 90, 95}) : "Test failed for allRounded";
+
+        // Test case with no grades
+        int[] emptyGrades = {};
+        int[] emptyRoundedGrades = Methode.allRounded(emptyGrades);
+        assert emptyRoundedGrades.length == 0 : "Test failed for allRounded (no grades)";
+    }
+
+    public static void testMaxRounded() {
+        // Test case with some grades
+        int[] grades = {45, 53, 62, 37, 88, 95};
+        int maxRoundedGrade = Methode.maxRounded(grades);
+        assert maxRoundedGrade == 95 : "Test failed for maxRounded (expected case)";
+
+        // Test case with no grades
+        int[] emptyGrades = {};
+        int maxRoundedEmpty = Methode.maxRounded(emptyGrades);
+        assert maxRoundedEmpty == 0 : "Test failed for maxRounded (no grades)";
+    }
+}
--- src/OperationsTest.java
+++ src/OperationsTest.java
@@ -1,57 +1,86 @@
 import java.util.Arrays;
+
 public class OperationsTest {
 
     public static void main() {
+        testAddition();
+        testSubtraction();
+        testMultiplication();
+        testDivision();
+        testEmptyArrayAddition();
+        testZeroArraySubtraction();
+        testLargeNumberAddition();
+        testDivisionByZero();
+    }
+
+    public static void testAddition() {
         int[] a = {1, 2, 3};
         int[] b = {4, 5, 6};
 
-        // Test addition
         int[] sum = Operations.add(a, b);
         int[] expectedSum = {5, 7, 9};
         checkResult("Addition Test", sum, expectedSum);
+    }
 
-        // Test subtraction
+    public static void testSubtraction() {
+        int[] a = {1, 2, 3};
+        int[] b = {4, 5, 6};
+
         int[] difference = Operations.subtract(a, b);
         int[] expectedDifference = {-3, -3, -3};
         checkResult("Subtraction Test", difference, expectedDifference);
+    }
 
-        // Test multiplication
+    public static void testMultiplication() {
+        int[] a = {1, 2, 3};
+
         int[] product = Operations.mult(a, 2);
         int[] expectedProduct = {2, 4, 6};
         checkResult("Multiplication Test", product, expectedProduct);
+    }
 
-        // Test division
+    public static void testDivision() {
+        int[] a = {1, 2, 3};
+
         int[] division = Operations.div(a, 2);
         int[] expectedDivision = {0, 1, 1};
         checkResult("Division Test", division, expectedDivision);
+    }
 
-        // Test unexpected cases
+    public static void testEmptyArrayAddition() {
+        int[] a = {1, 2, 3};
         int[] emptyArray = {};
-        int[] zeroArray = {0, 0, 0};
-        int[] largeNumber1 = {9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
-        int[] largeNumber2 = {1};
 
-        // Test empty array addition
         int[] emptyArraySum = Operations.add(emptyArray, a);
         int[] expectedEmptyArraySum = a;
         checkResult("Empty Array Addition", emptyArraySum, expectedEmptyArraySum);
+    }
 
-        // Test zero array subtraction
+    public static void testZeroArraySubtraction() {
+        int[] b = {4, 5, 6};
+        int[] zeroArray = {0, 0, 0};
+
         int[] zeroArrayDifference = Operations.subtract(zeroArray, b);
         int[] expectedZeroArrayDifference = {-4, -5, -6};
         checkResult("Zero Array Subtraction", zeroArrayDifference, expectedZeroArrayDifference);
+    }
 
-        // Test large number addition
+    public static void testLargeNumberAddition() {
+        int[] largeNumber1 = {9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
+        int[] largeNumber2 = {1};
+
         int[] expectedLargeSum = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         checkResult("Large Number Addition", Operations.add(largeNumber1, largeNumber2), expectedLargeSum);
+    }
 
-        // Test division by zero
+    public static void testDivisionByZero() {
+        int[] a = {1, 2, 3};
+
         int[] divisionByZero = Operations.div(a, 0);
-        int[] expectedDivisionByZero = a; // Division by zero should not modify the array
+        int[] expectedDivisionByZero = a;
         checkResult("Division by Zero", divisionByZero, expectedDivisionByZero);
     }
 
-    // Helper method to check the result
     private static void checkResult(String testName, int[] result, int[] expected) {
         System.out.println(testName);
         System.out.println("Result: " + Arrays.toString(result));
